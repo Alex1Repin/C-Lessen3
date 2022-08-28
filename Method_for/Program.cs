a8549fe4774c32356d721b9ff28c4533fa7a6a9b
Alex <al.repin.a@yandex.ru>
2022-08-28 17:42:58 +0300

    add Method_for

---
+++ Method_for/Program.cs
@@ -0,0 +1,22 @@
+﻿string Method_for(int count, string text)
+{
+    string result = string.Empty;
+    for(int i = 0; i < count; i ++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method_for(10, "z");
+//Console.WriteLine(res);
+
+for(int i = 2; i <= 10; i ++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}*{j}={i*j}");
+    }
+    Console.WriteLine();
+}
+    
+
